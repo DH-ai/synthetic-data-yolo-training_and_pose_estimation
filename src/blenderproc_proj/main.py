21e127bacc8bdf1e1ebf7c4745c794cf42998559
Dhruv Chaturvedi <dhruvastro67@gmail.com>
2026-06-09 11:15:03 +0530

    Add initial scene.py file for scene management functionality

--- src/blenderproc_proj/main.py
+++ src/blenderproc_proj/main.py
@@ -4,30 +4,26 @@ import os
 import numpy as np
 bproc.init()
 
-ASSET = os.path.join("/home/dhruv/obscureP/synthetic-data-yolo-training_and_pose_estimation", "assets", "models")
-
-
-SAVEASSETS = os.path.join(os.path.dirname(os.path.abspath(__file__)),"assets")
-
-
-OUTPUTPATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "output")
+# ASSET = os.path.join("/home/dhruv/obscureP/synthetic-data-yolo-training_and_pose_estimaDE
 # Load objects
-objs = bproc.loader.load_obj(os.path.join(ASSET, "triangle_shape.obj"))
-
-# Add light
-light = bproc.types.Light()
+objs = bproc.loader.load_obj(os.path.join("/home/dhruv/obscureP/synthetic-data-yolo-training_and_pose_estimation/blender_files","Scene2.obj")  t)
+# scene = bproc.loader.load_blend(os.path.join( "/home/dhruv/obscureP/synthetic-data-yolo-training_and_pose_estimation","blender_files","Scene2.blend"))
+    # /home/dhruv/obscureP/synthetic-data-yolo-training_and_pose_estimation/blender_files/Scene2.obj
+# Add lightB
+# light = bproc.types.Light()
 
 # Set camera
-bproc.camera.set_resolution(640, 480)
+# bproc.camera.set_resolution(640, 480)
 
 # Add camera poses
 
 
-cam_pose = bproc.math.build_transformation_mat([0, 0, 1], [0, 0])
-bproc.camera.add_camera_pose(cam_pose)
+# cam_pose = bproc.math.build_transformation_mat([0, 0, 1], [0, 0])
+# bproc.camera.add_camera_pose(cam_pose)
 # Render
-data = bproc.renderer.render()
+# data = bproc.renderer.render()
 
 # Write annotations
-bproc.writer.write_hdf5(os.path.join(OUTPUTPATH, "annotations.hdf5"), data)
+# bproc.writer.write_hdf5(os.path.join(OUTPUTPATH, "annotations.hdf5"), data)
 # bproc.writer.write_coco_annotations(os.path.join("/home/dhruv/obscureP/synthetic-data-yolo-training_and_pose_estimation", "assets", "annotations.json"), data)
+ 
